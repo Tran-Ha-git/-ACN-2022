192e9d023e5c5dc46c9247435f038d700f1dc13b
Tran-Ha-git <hochiminhvilanhtuuvidai@gmail.com>
2022-04-12 07:05:26 +0700

    setup

---
+++ src/main/java/com/web/dacn/service/IDemoService.java
@@ -0,0 +1,10 @@
+package com.web.dacn.service;
+
+import java.util.Optional;
+
+import com.web.dacn.dto.UserDto;
+import com.web.dacn.entity.User;
+
+public interface IDemoService {
+	UserDto getDemo();
+}
